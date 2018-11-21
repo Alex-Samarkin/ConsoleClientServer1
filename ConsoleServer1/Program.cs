bb7dac63e5d5dc48fa89e1f8ae739d95e4c6a340
alex <alex_samarkin@mail.ru>
2018-11-22 02:18:48 +0300

    1

--- ConsoleServer1/Program.cs
+++ ConsoleServer1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// подключение сокетов
 using System.Net.Sockets;
 
 namespace ConsoleServer1
