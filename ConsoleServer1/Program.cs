bc86193753a2d56d75d4ddb1b0d7f06479bdfea7
alex <alex_samarkin@mail.ru>
2018-11-22 02:26:39 +0300

    2

--- ConsoleServer1/Program.cs
+++ ConsoleServer1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 // подключение сокетов
@@ -12,6 +13,15 @@ namespace ConsoleServer1
     {
         static void Main(string[] args)
         {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            Console.WriteLine(ipEndPoint.ToString());
+
+
+            Console.ReadLine();
         }
     }
 }
