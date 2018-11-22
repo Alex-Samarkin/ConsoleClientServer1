598553f19cb04ca217bbf32b942f56137fe6e857
alex <alex_samarkin@mail.ru>
2018-11-22 04:19:31 +0300

    2

--- ConsoleClient1/Program.cs
+++ ConsoleClient1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +26,24 @@ namespace ConsoleClient1
             {
                 Console.ReadLine();
             }
+        }
+
+        public static void SendMessageFromSocket(int v)
+        {
+            // Буфер для входящих данных
+            byte[] bytes = new byte[1024];
+
+            // Соединяемся с удаленным устройством
+
+            // Устанавливаем удаленную точку для сокета
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Соединяем сокет с удаленной точкой
+            sender.Connect(ipEndPoint);
         }
     }
 }
