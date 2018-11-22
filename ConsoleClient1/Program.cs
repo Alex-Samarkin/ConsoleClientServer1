f46682b63fb61f1ee9fd23ee106f95bb6ebf5693
alex <alex_samarkin@mail.ru>
2018-11-22 04:06:15 +0300

    2

--- ConsoleClient1/Program.cs
+++ ConsoleClient1/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net.Sockets;
+
+
 namespace ConsoleClient1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                SendMessageFromSocket(11000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
